885fd0c45dfd0f381e5aeaf6fafe174c1884c7e3
bbpcoforge <bipin.pandey@coforge.com>
2022-12-30 18:37:04 +0530

    balance page content

--- account/src/components/Activity.js
+++ account/src/components/Activity.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Menu from './Menu';
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Menu from "./Menu";
 
 export default function Activity() {
-
   return (
-      <Grid container spacing={2}>
+    <Grid container spacing={2}>
       <Grid item xs={2}>
-            <Menu />
-        </Grid>
-        <Grid item xs={10}>
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="textPrimary"
-            gutterBottom
-          >
-            Account Activity
-          </Typography>
-        </Grid>
+        <Menu />
       </Grid>
+      <Grid item xs={10}>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          Account Activity
+        </Typography>
+      </Grid>
+    </Grid>
   );
 }
