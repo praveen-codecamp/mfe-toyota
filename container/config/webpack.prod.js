d18fdcfc7dec8ea9eb813c59dfb5d3d1c28245ce
vidhya-bahl-coforge-com <vidhya.bahl@coforge.com>
2023-01-02 11:40:47 +0530

    payment path corrected.

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
       remotes: {
         account: `account@http://account-nlb-bc46dacd7ae6e1f1.elb.ap-south-1.amazonaws.com/account/latest/remoteEntry.js`,
         auth: `auth@http://auth-nlb-469677bbd6307bc7.elb.ap-south-1.amazonaws.com/auth/latest/remoteEntry.js`,
-        payment: `payment@http://payment-nlb-f1cca66debff337c.elb.ap-south-1.amazonaws.com/payment/latestremoteEntry.js`,
+        payment: `payment@http://payment-nlb-f1cca66debff337c.elb.ap-south-1.amazonaws.com/payment/latest/remoteEntry.js`,
         preferences: `preferences@http://preference-nlb-a1aec725c352a61d.elb.ap-south-1.amazonaws.com/preferences/latest/remoteEntry.js`,
         //admin: `admin@${domain}/admin/latest/remoteEntry.js`,
       },
