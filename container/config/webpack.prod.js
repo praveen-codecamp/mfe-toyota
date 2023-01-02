90150f9a055106c7ddeda5bf4b69d5af3030068e
vidhya-bahl-coforge-com <vidhya.bahl@coforge.com>
2023-01-02 11:25:50 +0530

    aws changes committed

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -3,8 +3,6 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-const domain = process.env.PRODUCTION_DOMAIN;
-
 const prodConfig = {
   mode: 'production',
   output: {
@@ -15,11 +13,11 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        account: `account@${domain}/account/latest/remoteEntry.js`,
-        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
-        payment: `payment@${domain}/payment/latest/remoteEntry.js`,
-        preferences: `preferences@${domain}/preferences/latest/remoteEntry.js`,
-        admin: `admin@${domain}/admin/latest/remoteEntry.js`,
+        account: `account@http://account-nlb-bc46dacd7ae6e1f1.elb.ap-south-1.amazonaws.com/account/latest/remoteEntry.js`,
+        auth: `auth@http://auth-nlb-469677bbd6307bc7.elb.ap-south-1.amazonaws.com/auth/latest/remoteEntry.js`,
+        payment: `payment@http://payment-nlb-f1cca66debff337c.elb.ap-south-1.amazonaws.com/payment/latestremoteEntry.js`,
+        preferences: `preferences@http://preference-nlb-a1aec725c352a61d.elb.ap-south-1.amazonaws.com/preferences/latest/remoteEntry.js`,
+        //admin: `admin@${domain}/admin/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
