a38ecbfb1db086ae30db9b14f1579f0941762c60
bbpcoforge <bipin.pandey@coforge.com>
2023-01-03 18:45:31 +0530

    show ver in footer

--- account/src/App.js
+++ account/src/App.js
@@ -1,21 +1,37 @@
-import React from 'react';
-import { Switch, Route, Router } from 'react-router-dom';
+import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
 
-import Landing from './components/Landing';
-import Balance from './components/Balance';
-import Activity from  './components/Activity'
+import Landing from "./components/Landing";
+import Balance from "./components/Balance";
+import Activity from "./components/Activity";
 
+function Copyright() {
+  const packageJson = require("../package.json");
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."} {` ver ${packageJson.version}`}
+    </Typography>
+  );
+}
 export default ({ history }) => {
   return (
     <div>
-        <Router history={history}>
-          <Switch>
-            <Route exact path="/account/balance" component={Balance} />
-            <Route exact path="/account/activity" component={Activity} />
-            <Route path="/account" component={Landing} />
-            <Route path="/" component={Landing} />
-          </Switch>
-        </Router>
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/account/balance" component={Balance} />
+          <Route exact path="/account/activity" component={Activity} />
+          <Route path="/account" component={Landing} />
+          <Route path="/" component={Landing} />
+        </Switch>
+      </Router>
+      <Copyright />
     </div>
   );
 };
--- payment/src/App.js
+++ payment/src/App.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import { Switch, Route, Router } from 'react-router-dom';
+import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
 
-import Landing from './components/Landing';
-import Standard from './components/Standard';
-import Authorise from './components/Authorise';
+import Landing from "./components/Landing";
+import Standard from "./components/Standard";
+import Authorise from "./components/Authorise";
+
+function Copyright() {
+  const packageJson = require("../package.json");
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."} {` ver ${packageJson.version}`}
+    </Typography>
+  );
+}
 
 export default ({ history }) => {
   return (
     <div>
-        <Router history={history}>
-          <Switch>
-            <Route exact path="/payment" component={Landing} />
-            <Route exact path="/payment/standard" component={Standard} />
-            <Route exact path="/payment/authorise" component={Authorise} />
-            <Route path="/" component={Landing} />
-          </Switch>
-        </Router>
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/payment" component={Landing} />
+          <Route exact path="/payment/standard" component={Standard} />
+          <Route exact path="/payment/authorise" component={Authorise} />
+          <Route path="/" component={Landing} />
+        </Switch>
+      </Router>
+      <Copyright />
     </div>
   );
 };
