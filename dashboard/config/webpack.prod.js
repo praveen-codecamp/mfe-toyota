f435a07f03ee58545050984617c409fc4c6b5dcb
vidhya-bahl-coforge-com <vidhya.bahl@coforge.com>
2023-01-16 10:56:00 +0530

    dashboard App name updated.

--- dashboard/config/webpack.prod.js
+++ dashboard/config/webpack.prod.js
@@ -11,7 +11,7 @@ const prodConfig = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "dashboardApp",
+      name: "dashboard",
       filename: "remoteEntry.js",
       exposes: {
         "./DashboardApp": "./src/bootstrap",
