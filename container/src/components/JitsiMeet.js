fda42b3bd1ea47d66c950fe40034fb121b57b8f5
bbpcoforge <bipin.pandey@coforge.com>
2023-01-27 14:17:46 +0530

    dashbord remove left menu

--- container/src/components/JitsiMeet.js
+++ container/src/components/JitsiMeet.js
@@ -1,4 +1,5 @@
-import React, { useState, useParams } from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import Jitsi from "react-jitsi";
 
 const JitsiMeet = () => {
