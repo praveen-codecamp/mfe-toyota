097f68674e7cde730debf7d698db527590f07253
vidhya-bahl-coforge-com <vidhya.bahl@coforge.com>
2023-02-02 09:58:09 +0530

    setting up on S3

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -7,17 +7,17 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/",
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        account: `account@http://account-nlb-a5776d8e7193e62e.elb.ap-south-1.amazonaws.com/remoteEntry.js`,
-        auth: `auth@http://auth-nlb-7ffa94926ae2aa47.elb.ap-south-1.amazonaws.com/remoteEntry.js`,
-        payment: `payment@http://payment-nlb-44d05710ce8177ad.elb.ap-south-1.amazonaws.com/remoteEntry.js`,
-        preferences: `preferences@http://preference-nlb-a5ef5aa71dddcb2f.elb.ap-south-1.amazonaws.com/remoteEntry.js`,
-        dashboard: `dashboard@http://dashboard-nlb-c15fa8773c0ac683.elb.ap-south-1.amazonaws.com/remoteEntry.js`,
+        account: `account@${domain}/account/latest/remoteEntry.js`,
+        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+        payment: `payment@${domain}/payment/latest/remoteEntry.js`,
+        preferences: `preferences@${domain}/preferences/latest/remoteEntry.js`,
+        dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`,
         admin: `admin@http://localhost:3025/admin/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
