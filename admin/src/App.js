7011ee1147342a83c7de1d3ae356001e24120f6f
bbpcoforge <bipin.pandey@coforge.com>
2023-05-16 12:31:29 +0530

    nav in shared

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Switch, Route, Router } from "react-router-dom";
 import ThemeProvider from "../../shared/theme";
+import Nav from "../../shared/nav";
+import { navConfig } from "./navConfig";
 import Landing from "./components/Landing";
 
 export default ({ history }) => {
+  const [open, setOpen] = React.useState(false);
   return (
     <div style={{ marginTop: 64 }}>
       <ThemeProvider>
         <Router history={history}>
+          <Nav
+            openNav={open}
+            onCloseNav={() => setOpen(false)}
+            navConfig={navConfig}
+          />
           <Switch>
             <Route exact path="/admin" component={Landing} />
             <Route path="/" component={Landing} />
---
+++ shared/nav/nav-section/styles.js
@@ -0,0 +1,28 @@
+import React from "react";
+// @mui
+import { styled } from "@mui/material/styles";
+import { ListItemIcon, ListItemButton } from "@mui/material";
+
+// ----------------------------------------------------------------------
+
+export const StyledNavItem = styled((props) => (
+  <ListItemButton disableGutters {...props} />
+))(({ theme }) => ({
+  ...theme.typography.body2,
+  height: 26,
+  position: "relative",
+  textTransform: "capitalize",
+  color: theme.palette.text.secondary,
+  borderRadius: theme.shape.borderRadius,
+  fontSize: ".7rem",
+  marginTop: 10,
+}));
+
+export const StyledNavItemIcon = styled(ListItemIcon)({
+  width: 22,
+  height: 22,
+  color: "inherit",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
