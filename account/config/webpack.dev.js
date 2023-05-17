aa9151227158fa9623917020c1a21c6f7f8de8b8
vidhya-bahl-coforge-com <vidhya.bahl@coforge.com>
2023-05-17 15:11:39 +0530

    dummy commit

--- account/config/webpack.dev.js
+++ account/config/webpack.dev.js
@@ -11,7 +11,7 @@ const devConfig = {
   devServer: {
     port: 3021,
     historyApiFallback: {
-      index: "index-dev.html",
+      index: "index-dev.html", 
     },
     hot: false,
     liveReload: true,
