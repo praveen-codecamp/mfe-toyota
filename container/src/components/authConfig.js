689c0b6ab64c229b807799b48de7054e11db92bb
vidhya-bahl-coforge-com <vidhya.bahl@coforge.com>
2023-05-18 22:19:00 +0530

    revert groups in scopes to test

--- container/src/components/authConfig.js
+++ container/src/components/authConfig.js
@@ -3,7 +3,7 @@ const configs = {
     clientId: "0oa5is5r7eaChtf9E697",
     issuer: "https://trial-3576565.okta.com/oauth2/default",
     redirectUri: window.location.origin + "/dashboard",
-    scopes: ["openid", "profile", "email", "groups"],
+    scopes: ["openid", "profile", "email"],
     pkce: true,
     disableHttpsCheck: false,
     useInteractionCode: false,
