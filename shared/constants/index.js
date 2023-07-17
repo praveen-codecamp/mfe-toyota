f6949211afbfd6a5121dc8f01d6fa39cf3a0e430
bbpcoforge <bipin.pandey@coforge.com>
2023-07-17 14:04:09 +0530

    live api

--- shared/constants/index.js
+++ shared/constants/index.js
@@ -1,5 +1,5 @@
 export const defaultCurrency = { code: "AED", symbol: "AED" }; //{code:"USD", symbol:"$"};
-export const accessControlAPI = "http://localhost:8080/api"; //"http://54.85.5.176:8080/api";
+export const accessControlAPI = "https://54.196.250.10/api"; //"http://54.85.5.176:8080/api";
 export const accounts = [
   {
     name: "Jon Yml",
